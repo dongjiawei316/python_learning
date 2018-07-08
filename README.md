0ff16c78f1af568d765902540d26e12784f9ebfd
dongjiawei316 <dongjiawei316@126.com>
2018-07-08 19:20:42 +0800

    增加了离散的点的画图，

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# python learnin 
+# python learning 
 学习python时写的一些例子，源自python编程从入门到实践，开发环境是pycharm
--- scatter_squares.py
+++ scatter_squares.py
@@ -1,20 +1,19 @@
 #-*- coding:utf-8 -*-
 import matplotlib.pyplot as plt
 s = "从入门到实践的例子-15"
-
 print(s)
-squares = [1, 4, 9, 16, 25, 36]
-input_values = [1, 2, 3, 4, 5, 6]
 
-plt.plot(input_values, squares, linewidth = 5)
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, c=(0.8,0,0), edgecolor='none', s = 4)
 
 #设置图标标题，并给坐标轴加上标签
 plt.title("Square Numbers", fontsize = 24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize = 14)
 
-#设置标记刻度标记的大小
-plt.tick_params(axis='both', labelsize=14)
-
+#设置每一个坐标轴的取值范围
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
